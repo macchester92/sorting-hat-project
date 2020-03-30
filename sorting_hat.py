414ed963649c748307df803e9749fb3970123110
macchester92 <radchenkovictor@yahoo.com>
2020-03-30 22:53:55 +0400

    testing gitignore

--- sorting_hat.py
+++ sorting_hat.py
@@ -1,7 +1,22 @@
 import os
 import sys
-import hashlib
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
-md5 = hashlib.md5()
 
-file = 
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while observer.isAlive():
+            observer.join(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
